785171a3d7885497e628dd69a03cab6b5aad1e4a
nirkov <nir.kovrovski@gmail.com>
2019-07-31 15:52:41 +0300

    Flower State Machine

--- src/flower.erl
+++ src/flower.erl
@@ -6,11 +6,11 @@
 %%% @end
 %%% Created : 30. Jul 2019 19:43
 %%%-------------------------------------------------------------------
--module('Flower').
+-module(flower).
 -author("nirkov").
 
 %% API
--export([flowerAsStateMachine/3, getTolarableTime/1, getRandomStatus/0]).
+-export([flowerAsStateMachine/3, getTolarableTime/1, getRandomStatus/0, tests/0]).
 
 -record(flower,{id, status, pointsLifeTime}).
 newFlower(ID, Status, PointsLifeTime)->
@@ -29,14 +29,16 @@ flowerAsStateMachine(Flower=#flower{id=ID, status=Status, pointsLifeTime = Point
 
   receive
     updateStatus ->
+      print("Entered updateState"),
       % Choosing new mode randomly
       NewStatus      = getRandomStatus(),
+      print("New Status = ", NewStatus),
 
       % Create new record
       NewStateFlower = newFlower(ID, NewStatus, PointsLifeTime),
 
       % Send report to the server about status changing.
-      gardenGenServer:cast(MyGardenServerPID, {updateFlowerStatus, NewStateFlower}),
+      %TODO gardenGenServer:cast(MyGardenServerPID, {updateFlowerStatus, NewStateFlower}),
 
       flowerAsStateMachine(NewStateFlower, 0, MyGardenServerPID);
 
@@ -48,23 +50,29 @@ flowerAsStateMachine(Flower=#flower{id=ID, status=Status, pointsLifeTime = Point
       NewStateFlower = newFlower(ID, normal, PointsLifeTime),
 
       % Change the status of the flower in the server to normal.
-      gardenGenServer:cast(MyGardenServerPID, {updateFlowerStatus, NewStateFlower}),
+      %TODO gardenGenServer:cast(MyGardenServerPID, {updateFlowerStatus, NewStateFlower}),
       flowerAsStateMachine(NewStateFlower, 0, MyGardenServerPID)
 
-    after 10 ->
+    after 1000 ->
+    print("Entered after"),
+    print("Status = ", Status),
+    print("Start Time Problem", StartTimeProblem),
     if
       % Send trigger to himself to change the status if it status is nurmal and
       % do it only one time when the startTimeProblem is 0 (from the handleProblem state).
       Status =:= normal and StartTimeProblem =:= 0 ->
+        print("Entered after and send trigger to updateState"),
         ID ! updateStatus,
         flowerAsStateMachine(Flower, -1, MyGardenServerPID);
 
       true ->
         ToleranceTime = getTolarableTime(Status),
+        print("Tolerance Time", ToleranceTime),
+
         if
           % Check if the The tolerable time for the given problem is less than the time
           % it took the gardener to handle the problem and in this case kill the flower.
-          StartTimeProblem > ToleranceTime ->  gardenGenServer:cast(MyGardenServerPID, {kill, Flower});
+          StartTimeProblem > ToleranceTime -> print("kill"); %TODO gardenGenServer:cast(MyGardenServerPID, {kill, Flower});
 
           % Otherwise the gardener still has not addressed the problem and we are increment
           % the counter of time by 1.
@@ -95,3 +103,21 @@ getRandomStatus()->
     RandomStatus > 30 and RandomStatus < 10 -> fertalize
   end.
 
+tests()->
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()),
+  print("Status", getRandomStatus()).
+%%  NewFlower = newFlower(a, normal, 0),
+%%  register(a, spawn(flower, flowerAsStateMachine, [NewFlower, -1, 1])).
+
+
+print(Msg, Object)-> io:fwrite(Msg ++ "~p~n", [Object]).
+print(Msg)-> io:fwrite(Msg ++ "~n").
