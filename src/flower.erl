9b634e0d3a90776d421313b84784d26ae8d2e2d4
nirkov <nir.kovrovski@gmail.com>
2019-08-05 16:10:10 +0300

    FIX BUG - FLOWER

--- src/flower.erl
+++ src/flower.erl
@@ -10,7 +10,7 @@
 -author("nirkov").
 
 %% API
--export([flowerAsStateMachine/2, getTolarableTime/1, getRandomStatus/0, tests/0, print/1, print/2]).
+-export([flowerAsStateMachine/1]).
 -include("globalVariables.hrl").
 
 newFlower(ID, Type, Status, TimeSinceProblem, GardenerID, GardenID, X, Y)->
@@ -41,7 +41,7 @@ flowerAsStateMachine(Flower=#flower{id=ID, type =Type , status=Status, timeSince
 
     handleProblem ->
       % Time the gardener handle the problem
-      timer:sleep(?handle), %TODO: CHECK WITH NAOR
+      timer:sleep(?handle),
 
       % New record flower with normal state.
       NewStateFlower = newFlower(ID, Type, normal, 0, GardenerID, GardenID, X, Y),
@@ -57,7 +57,7 @@ flowerAsStateMachine(Flower=#flower{id=ID, type =Type , status=Status, timeSince
     % do it only one time when the startTimeProblem is 0 (from the handleProblem state).
       (Status =:= normal) and (TimeSinceProblem =:= 0) ->
         ID ! updateStatus,
-        flowerAsStateMachine(Flower#{timeSinceProblem = -1});
+        flowerAsStateMachine(Flower#flower{timeSinceProblem = -1});
 
       (Status =:= normal) and (TimeSinceProblem =/= 0)->
         flowerAsStateMachine(Flower);
@@ -68,7 +68,7 @@ flowerAsStateMachine(Flower=#flower{id=ID, type =Type , status=Status, timeSince
         % Check if the The tolerable time for the given problem is less than the time
         % it took the gardener to handle the problem and in this case kill the flower.
           TimeSinceProblem > ToleranceTime ->
-            gen_server:cast(getGardenName(GardenID), {flowerDie, Flower#{status = kill}});
+            gen_server:cast(getGardenName(GardenID), {flowerDie, Flower#flower{status = kill}});
 
         % Otherwise the gardener still has not addressed the problem and we are increment
         % the counter of time by 1.
@@ -92,13 +92,6 @@ getGardenName(Number) ->
       {global,?garden4Name}
   end.
 
-getTolarableTime(Status)->
-  case Status of
-    pests_ant    -> ?pestsTime;
-    pests_purple -> ?pestsTime;
-    pests_green  -> ?pestsTime;
-    wilted       -> ?waterTime
-  end.
 
 getRandomStatus()->
   {T1,T2,T3} = now(),
@@ -112,11 +105,6 @@ getRandomStatus()->
     RandomStatus < 120 - ?level -> normal
   end.
 
-
-print(Msg, Object)-> io:fwrite(Msg ++ "~p~n", [Object]).
-print(Msg)-> io:fwrite(Msg ++ "~n").
-
-
 getTolarableTime(Status)->
   case Status of
     pests_ant    -> ?pestsTime;
@@ -125,7 +113,8 @@ getTolarableTime(Status)->
     wilted       -> ?waterTime
   end.
 
-
+print(Msg, Object)-> io:fwrite(Msg ++ "~p~n", [Object]).
+print(Msg)-> io:fwrite(Msg ++ "~n").
 
 tests()->ok.
 %%  print("Start tests"),
